e7e75680edb43ffbf152ee19374b83e41b581db9
sslampa <sslampa@gmail.com>
2018-01-06 10:03:43 -0800

    Finish tutorial

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
 namespace simple_csharp_app
 {
@@ -23,6 +25,7 @@ namespace simple_csharp_app
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
         }
 
